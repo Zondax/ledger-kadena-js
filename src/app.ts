ece2528bee1652c20ff959405633f4556f9104e6
abenso <andresbenso@gmail.com>
2024-09-06 11:21:37 -0300

    implement transfer command

--- src/app.ts
+++ src/app.ts
@@ -21,10 +21,17 @@ import BaseApp, {
   processResponse,
 } from "@zondax/ledger-js";
 
-import { ResponseAddress } from "./types";
+import {
+  ResponseAddress,
+  ResponseSign,
+  ResponseSignTransfer,
+  TransferTxParams,
+  TransferCrossChainTxParams,
+  TransferTxType,
+  maxLengths,
+} from "./types";
 import { P1_VALUES, PUBKEYLEN } from "./consts";
-
-import { ResponseSign } from "./types";
+import { blake2bFinal, blake2bInit, blake2bUpdate } from "blakejs";
 
 export class KadenaApp extends BaseApp {
   static _INS = {
@@ -107,10 +114,16 @@ export class KadenaApp extends BaseApp {
     }
   }
 
-  async signHash(path: BIP32Path, hash: string | Buffer | Uint8Array): Promise<ResponseSign> {
-
-    const rawHash = typeof hash == "string" ?
-    (hash.length == 64 ? Buffer.from(hash, "hex") : Buffer.from(hash, "base64")) : Buffer.from(hash);
+  async signHash(
+    path: BIP32Path,
+    hash: string | Buffer | Uint8Array,
+  ): Promise<ResponseSign> {
+    const rawHash =
+      typeof hash == "string"
+        ? hash.length == 64
+          ? Buffer.from(hash, "hex")
+          : Buffer.from(hash, "base64")
+        : Buffer.from(hash);
     if (rawHash.length != 32) {
       throw new TypeError("Hash is not 32 bytes");
     } else {
@@ -122,7 +135,7 @@ export class KadenaApp extends BaseApp {
           chunks.length,
           chunks[0],
         );
-  
+
         for (let i = 1; i < chunks.length; i += 1) {
           signatureResponse = await this.signSendChunk(
             this.INS.SIGN_HASH,
@@ -139,4 +152,327 @@ export class KadenaApp extends BaseApp {
       }
     }
   }
+
+  async signTransferTx(
+    path: BIP32Path,
+    params: TransferTxParams,
+  ): Promise<ResponseSignTransfer> {
+    var p1 = params as TransferCrossChainTxParams;
+    p1.recipient_chainId = 0; // Ignored by Ledger App
+    return await this.signTxInternal(path, p1, TransferTxType.TRANSFER);
+  }
+
+  async signTransferCreateTx(
+    path: BIP32Path,
+    params: TransferTxParams,
+  ): Promise<ResponseSignTransfer> {
+    var p1 = params as TransferCrossChainTxParams;
+    p1.recipient_chainId = 0; // Ignored by Ledger App
+    return await this.signTxInternal(path, p1, TransferTxType.TRANSFER_CREATE);
+  }
+
+  async signTransferCrossChainTx(
+    path: BIP32Path,
+    params: TransferCrossChainTxParams,
+  ): Promise<ResponseSignTransfer> {
+    if (params.chainId == params.recipient_chainId)
+      throw new TypeError(
+        "Recipient chainId is same as sender's in a cross-chain transfer",
+      );
+    return await this.signTxInternal(
+      path,
+      params,
+      TransferTxType.TRANSFER_CROSS_CHAIN,
+    );
+  }
+
+  private async signTxInternal(
+    path: BIP32Path,
+    params: TransferCrossChainTxParams,
+    txType: TransferTxType,
+  ): Promise<ResponseSignTransfer> {
+    // Use defaults if value not specified
+    const t: Date = new Date();
+
+    this.checkTransferTxParamsSanity(params);
+
+    const recipient = params.recipient.startsWith("k:")
+      ? params.recipient.substring(2)
+      : params.recipient;
+    const namespace_ = params.namespace === undefined ? "" : params.namespace;
+    const module_ = params.module === undefined ? "" : params.module;
+    const amount = convertDecimal(params.amount);
+    const gasPrice = params.gasPrice === undefined ? "1.0e-6" : params.gasPrice;
+    const gasLimit = params.gasLimit === undefined ? "2300" : params.gasLimit;
+    const creationTime =
+      params.creationTime === undefined
+        ? Math.floor(t.getTime() / 1000)
+        : params.creationTime;
+    const ttl = params.ttl === undefined ? "600" : params.ttl;
+    const nonce = params.nonce === undefined ? "" : params.nonce;
+
+    const txTypeB = Buffer.alloc(1);
+    txTypeB.writeInt8(txType);
+    // These are just squashed together
+    const payload = Buffer.concat([
+      txTypeB,
+      textPayload(recipient),
+      textPayload(params.recipient_chainId.toString()),
+      textPayload(params.network),
+      textPayload(amount),
+      textPayload(namespace_),
+      textPayload(module_),
+      textPayload(gasPrice),
+      textPayload(gasLimit),
+      textPayload(creationTime.toString()),
+      textPayload(params.chainId.toString()),
+      textPayload(nonce),
+      textPayload(ttl),
+    ]);
+
+    const chunks = this.prepareChunks(path, payload);
+    try {
+      let signatureResponse = await this.signSendChunk(
+        this.INS.SIGN_TRANSFER_TX,
+        1,
+        chunks.length,
+        chunks[0],
+      );
+
+      for (let i = 1; i < chunks.length; i += 1) {
+        signatureResponse = await this.signSendChunk(
+          this.INS.SIGN_TRANSFER_TX,
+          1 + i,
+          chunks.length,
+          chunks[i],
+        );
+      }
+
+      const pubkey = await this.getAddressAndPubKey(path, false);
+      const json = this.buildTransferJson(
+        params,
+        txType,
+        pubkey.pubkey.toString("hex"),
+        creationTime,
+      );
+
+      const context = blake2bInit(32);
+      const jsonBuff = Buffer.from(json);
+      blake2bUpdate(context, jsonBuff);
+      const hash_bytes = Buffer.from(blake2bFinal(context));
+
+      var hash = Buffer.from(hash_bytes)
+        .toString("base64")
+        .replace(/\+/g, "-")
+        .replace(/\//g, "_")
+        .replace(/=/g, "");
+
+      return {
+        signature: signatureResponse.readBytes(signatureResponse.length()),
+        cmd: json,
+        hash: hash,
+      };
+    } catch (e) {
+      throw processErrorResponse(e);
+    }
+  }
+
+  private checkTransferTxParamsSanity(
+    params: TransferCrossChainTxParams,
+  ): void {
+    const recipient = params.recipient.startsWith("k:")
+      ? params.recipient.substring(2)
+      : params.recipient;
+    if (!recipient.match(/[0-9A-Fa-f]{64}/g))
+      throw new TypeError(
+        "Recipient should be a hex encoded pubkey or 'k:' address",
+      );
+
+    const namespace_ = params.namespace === undefined ? "" : params.namespace;
+    const module_ = params.module === undefined ? "" : params.module;
+    if (namespace_ != "" && module_ == "")
+      throw new TypeError(
+        "Along with 'namespace' 'module' need to be specified",
+      );
+
+    let isNaN_ = (v: unknown) => {
+      if (v === undefined) return false;
+      return isNaN(v as unknown as number);
+    };
+    if (isNaN_(params.amount)) throw new TypeError("amount is non a number");
+    if (isNaN_(params.gasPrice))
+      throw new TypeError("gasPrice is non a number");
+    if (isNaN_(params.gasLimit))
+      throw new TypeError("gasLimit is non a number");
+    if (isNaN_(params.creationTime))
+      throw new TypeError("creationTime is non a number");
+    if (isNaN_(params.ttl)) throw new TypeError("ttl is non a number");
+
+    for (const [key, maxLength] of Object.entries(maxLengths)) {
+      const value =
+        key === "recipient" ? recipient : params[key as keyof typeof params];
+      const strValue = (value || "").toString();
+
+      if (key === "recipient" && strValue.length !== maxLength) {
+        throw new TypeError(
+          `${key} should be exactly ${maxLength} characters long`,
+        );
+      } else if (strValue.length > maxLength) {
+        throw new TypeError(`${key} should be ${maxLength} characters or less`);
+      }
+    }
+  }
+
+  private buildTransferJson(
+    params: TransferCrossChainTxParams,
+    txType: number,
+    pubkey: string,
+    creationTime: number,
+  ): string {
+    // Build the JSON, exactly like the Ledger app
+    const recipient = params.recipient.startsWith("k:")
+      ? params.recipient.substring(2)
+      : params.recipient;
+    const namespace_ = params.namespace === undefined ? "" : params.namespace;
+    const module_ = params.module === undefined ? "" : params.module;
+
+    var cmd = '{"networkId":"' + params.network + '"';
+    if (txType == 0) {
+      cmd += ',"payload":{"exec":{"data":{},"code":"';
+      if (namespace_ == "") {
+        cmd += "(coin.transfer";
+      } else {
+        cmd += "(" + namespace_ + "." + module_ + ".transfer";
+      }
+      cmd += ' \\"k:' + pubkey + '\\"';
+      cmd += ' \\"k:' + recipient + '\\"';
+      cmd += " " + params.amount + ')"}}';
+      cmd += ',"signers":[{"pubKey":"' + pubkey + '"';
+      cmd +=
+        ',"clist":[{"args":["k:' +
+        pubkey +
+        '","k:' +
+        recipient +
+        '",' +
+        params.amount +
+        "]";
+      if (namespace_ == "") {
+        cmd += ',"name":"coin.TRANSFER"},{"args":[],"name":"coin.GAS"}]}]';
+      } else {
+        cmd +=
+          ',"name":"' +
+          namespace_ +
+          "." +
+          module_ +
+          '.TRANSFER"},{"args":[],"name":"coin.GAS"}]}]';
+      }
+    } else if (txType == 1) {
+      cmd += ',"payload":{"exec":{"data":{';
+      cmd += '"ks":{"pred":"keys-all","keys":["' + recipient + '"]}';
+      cmd += '},"code":"';
+      if (namespace_ == "") {
+        cmd += "(coin.transfer-create";
+      } else {
+        cmd += "(" + namespace_ + "." + module_ + ".transfer-create";
+      }
+      cmd += ' \\"k:' + pubkey + '\\"';
+      cmd += ' \\"k:' + recipient + '\\"';
+      cmd += ' (read-keyset \\"ks\\")';
+      cmd += " " + params.amount + ')"}}';
+      cmd += ',"signers":[{"pubKey":"' + pubkey + '"';
+      cmd +=
+        ',"clist":[{"args":["k:' +
+        pubkey +
+        '","k:' +
+        recipient +
+        '",' +
+        params.amount +
+        "]";
+      if (namespace_ == "") {
+        cmd += ',"name":"coin.TRANSFER"},{"args":[],"name":"coin.GAS"}]}]';
+      } else {
+        cmd +=
+          ',"name":"' +
+          namespace_ +
+          "." +
+          module_ +
+          '.TRANSFER"},{"args":[],"name":"coin.GAS"}]}]';
+      }
+    } else {
+      cmd += ',"payload":{"exec":{"data":{';
+      cmd += '"ks":{"pred":"keys-all","keys":["' + recipient + '"]}';
+      cmd += '},"code":"';
+      if (namespace_ == "") {
+        cmd += "(coin.transfer-crosschain";
+      } else {
+        cmd += "(" + namespace_ + "." + module_ + ".transfer-crosschain";
+      }
+      cmd += ' \\"k:' + pubkey + '\\"';
+      cmd += ' \\"k:' + recipient + '\\"';
+      cmd += ' (read-keyset \\"ks\\")';
+      cmd += ' \\"' + params.recipient_chainId.toString() + '\\"';
+      cmd += " " + params.amount + ')"}}';
+      cmd += ',"signers":[{"pubKey":"' + pubkey + '"';
+      cmd +=
+        ',"clist":[{"args":["k:' +
+        pubkey +
+        '","k:' +
+        recipient +
+        '",' +
+        params.amount +
+        ',"' +
+        params.recipient_chainId.toString() +
+        '"]';
+      if (namespace_ == "") {
+        cmd +=
+          ',"name":"coin.TRANSFER_XCHAIN"},{"args":[],"name":"coin.GAS"}]}]';
+      } else {
+        cmd +=
+          ',"name":"' +
+          namespace_ +
+          "." +
+          module_ +
+          '.TRANSFER_XCHAIN"},{"args":[],"name":"coin.GAS"}]}]';
+      }
+    }
+    cmd += ',"meta":{"creationTime":' + creationTime.toString();
+    cmd +=
+      ',"ttl":' +
+      params.ttl +
+      ',"gasLimit":' +
+      params.gasLimit +
+      ',"chainId":"' +
+      params.chainId.toString() +
+      '"';
+    cmd +=
+      ',"gasPrice":' +
+      params.gasPrice +
+      ',"sender":"k:' +
+      pubkey +
+      '"},"nonce":"' +
+      params.nonce +
+      '"}';
+
+    return cmd;
+  }
 }
+
+function textPayload(txt: string): Buffer {
+  // 1 byte: length
+  const payload = Buffer.alloc(1 + txt.length);
+  payload[0] = txt.length;
+  payload.write(txt, 1, "utf-8");
+  return payload;
+}
+
+const convertDecimal = (decimal: number | string): string => {
+  const decimalStr = decimal.toString();
+  if (decimalStr.includes(".")) {
+    return decimalStr;
+  }
+  const numValue = Number(decimalStr);
+  if (Number.isInteger(numValue)) {
+    return `${decimalStr}.0`;
+  }
+  return decimalStr;
+};
--- src/types.ts
+++ src/types.ts
@@ -6,3 +6,47 @@ export interface ResponseAddress {
 export interface ResponseSign {
   signature: Buffer;
 }
+
+export interface ResponseSignTransfer extends ResponseSign {
+  cmd: string;
+  hash: string;
+}
+
+export enum TransferTxType {
+  TRANSFER = 0,
+  TRANSFER_CREATE = 1,
+  TRANSFER_CROSS_CHAIN = 2,
+}
+export interface TransferTxParams {
+  path?: string;
+  namespace?: string;
+  module?: string;
+  recipient: string;
+  amount: string;
+  chainId: number;
+  network: string;
+  gasPrice?: string;
+  gasLimit?: string;
+  creationTime?: number;
+  ttl?: string;
+  nonce?: string;
+}
+
+export interface TransferCrossChainTxParams extends TransferTxParams {
+  recipient_chainId: number;
+}
+
+export const maxLengths = {
+  recipient: 64,
+  namespace: 16,
+  module: 32,
+  recipient_chainId: 2,
+  network: 20,
+  amount: 32,
+  gasPrice: 20,
+  gasLimit: 10,
+  creationTime: 12,
+  chainId: 2,
+  nonce: 32,
+  ttl: 20,
+};
